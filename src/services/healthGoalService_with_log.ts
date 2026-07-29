cdbb7fb40b2394bf6b15f3d4ca20cf549efabadc
afshargithub <afsharrad_eedu@yahoo.com>
2026-07-29 23:30:53 +0330

    fix API import 4

--- src/services/healthGoalService_with_log.ts
+++ src/services/healthGoalService_with_log.ts
@@ -1,13 +1,3 @@
-// import type { HealthGoal } from "../types/HealthGoal";
-// import { API_BASE_URL } from "../config/api";
-// export async function getHealthGoals(): Promise<HealthGoal[]> {
-//     const response = await fetch(
-//         `${API_BASE_URL}/api/health/health-goals/`
-//     );
-//     const text = await response.text();
-//     return JSON.parse(text);
-// }
-// .........................
 import type { HealthGoal } from "../types/HealthGoal";
 import api from "../config/api";
 export async function getHealthGoals(): Promise<HealthGoal[]> {
